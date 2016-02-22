df18fd74a5427d440ebfa07e1a253ed29366a6ef
Glyne J. Gittens <Jgit121@gmail.com>
2016-02-22 17:00:48 -0500

    Added the honor code

--- Inventory.h
+++ Inventory.h
@@ -2,6 +2,24 @@
 #define __INVENTORY_H_INCLUDED__
 #endif
 
+/* On my honor,
+
+The following program is a result of personal e#ffort.
+ I have not discussed with anyone other than my instructor 
+ or any appropriate person in charge of the class. I have 
+ not used, or slightly modified code or portion of code 
+ from another student, or an unauthorized source.
+
+If any C++ language code or documentation used in my program 
+was obtained from another source, such as a textbook or course 
+notes, that has been clearly noted with a proper citation in 
+the comments of my program. I have not designed this program 
+in such a way as to defeat or interfere with the normal operation
+of the eniac system or cslab machines at Hunter College .
+
+< Glyne J. Gittens , Shawn M. Grauel>
+
+*/
 class Inventory {
  public:
 
